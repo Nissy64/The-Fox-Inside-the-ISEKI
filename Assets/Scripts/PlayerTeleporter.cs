91341fdd1629afe02a56f0ff2404a5d7d8bd61af
Nissy64 <a2480goood@gmail.com>
2022-10-11 19:52:55 +0900

    add teleporter

---
+++ Assets/Scripts/PlayerTeleporter.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeleporter : MonoBehaviour
+{
+    private GameObject currentTeleporter;
+
+    public float teleportCoolDown = 1;
+    private float teleportCoolDownCounter;
+
+    void Awake() 
+    {
+        ResetCoolDownTimer();
+    }
+
+    private void Update() 
+    {
+        TeleportCoolTime();
+
+        if(teleportCoolDownCounter == 0)
+        {
+            Teleport();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.CompareTag("Teleporter"))
+        {
+            currentTeleporter = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if(!other.CompareTag("Teleporter")) return;
+        if(other.gameObject == currentTeleporter)
+        {
+            currentTeleporter = null;
+        }
+    }
+
+    private void Teleport()
+    {
+        if(currentTeleporter != null)
+        {
+            Teleporter teleporter = currentTeleporter.GetComponent<Teleporter>();
+
+            transform.position = teleporter.GetDestination().position;
+            transform.rotation = teleporter.GetDestination().rotation;
+            ResetCoolDownTimer();
+        }
+    }
+
+    private void TeleportCoolTime()
+    {
+        if(teleportCoolDownCounter > 0)
+        {
+            teleportCoolDownCounter -= Time.deltaTime;
+        }
+
+        if(teleportCoolDownCounter < 0)
+        {
+            teleportCoolDownCounter = 0;
+        }
+    }
+
+    private void ResetCoolDownTimer()
+    {
+        teleportCoolDownCounter = teleportCoolDown;
+    }
+}
